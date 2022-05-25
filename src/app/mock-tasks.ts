41eef9d532a65e9d1599d136b447174dab6c6d2e
jabfry <jabfryyy@gmail.com>
2022-05-25 01:17:25 -0300

    Agregamos la simulacion de base de datos, mock y el interface tasks

---
+++ src/app/mock-tasks.ts
@@ -0,0 +1,27 @@
+import {Tasks} from "./tasks"
+export const TASK: Tasks[] = [
+    {
+        id: 1,
+        text: "Terminar taskList",
+        day: "25/05/22",
+        reminder: true
+    },
+    {
+        id: 2,
+        text: "Terminar front del portfolio",
+        day: "25/05/22",
+        reminder: true
+    },
+    {
+        id: 3,
+        text: "Terminar base de datos",
+        day: "25/05/22",
+        reminder: true
+    },
+    {
+        id: 1,
+        text: "Terminar POO",
+        day: "25/05/22",
+        reminder: false
+    },
+]
